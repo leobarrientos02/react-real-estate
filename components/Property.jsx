b33b8fae3ce6860a4df541b081ac970e3b2a6794
Your Name <leobarrientos02@gmail.com>
2022-02-06 22:55:39 -0500

    property component

---
+++ components/Property.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Box, Flex, Avatar, Text } from "@chakra-ui/react";
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from "millify";
+
+// Image
+import DefaultImage from '../assets/images/house.jpg';
+
+const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId } }) => (
+    <Link href={`/property/${externalId}`} passHref >
+        <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0' justifyContent='flex-start' cursor='pointer' >
+            <Box>
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage } width={400} height={260} alt='house' />
+            </Box>
+            <Box w='full'>
+                <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+                    <Flex alignItems='center'>
+                        <Box paddingRight='3' color='green.400'>
+                            {isVerified && <GoVerified color="green" />}
+                        </Box>
+                        <Text fontWeight='bold' fontSize='lg'>USD ${millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                </Flex>
+            </Box>
+        </Flex>
+    </Link>
+);
+export default Property;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-//API
+// API Service
 import { baseUrl, fetchApi } from "../utils/fetchApi";
+
+// Components
+import Property from "../components/Property";
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
@@ -34,7 +37,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4" 
       />
       <Flex flexWrap='wrap'>
-
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
       <Banner 
         purpose="BUY A HOME"
@@ -45,8 +48,8 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         buttonText="Explore Buying"
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008" 
-      />      
-
+      />
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}      
     </Box>
   )
 }
