a063e38d9d3966bf863fcf59a4dd10ef44f63c60
Your Name <leobarrientos02@gmail.com>
2022-02-08 15:31:50 -0500

    Search Filters component

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,16 @@
+
+import { useEffect, useState } from "react";
+import { Flex, Box, Text, Spinner, Icon, Button, Select } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from 'react-icons/md';
+import Image from "next/image";
+
+const SearchFilters = () => {
+    return(
+        <div>
+            Leo
+        </div>
+    )
+}
+
+export default SearchFilters;
--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
 
+// Components
+import SearchFilters from "../components/SearchFilters";
+
 const search = () => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
@@ -13,9 +16,19 @@ const search = () => {
             <Flex
                 cursor='pointer'
                 bg='gray.100'
+                borderBottom='1px'
+                borderColor='gray.200'
+                p='2'
+                fontWeight='black'
+                fontSize='lg'
+                justifyContent='center'
+                alignItems='center'
+                onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
             >
-                <Text>Search</Text>
+                <Text>Search Property By Filters</Text>
+                <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
+            {searchFilters && <SearchFilters/>}
         </Box>
     )
 }
