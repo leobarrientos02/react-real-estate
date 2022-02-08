0a023303282a0252f0145e14b5bf8dd8a29d404b
Your Name <leobarrientos02@gmail.com>
2022-02-08 16:10:31 -0500

    svg and filter page working yet still need to add the filter funtion

--- pages/search.js
+++ pages/search.js
@@ -4,10 +4,17 @@ import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
 
+// Image
+import noresult from '../assets/images/noresult.svg';
+
 // Components
 import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
 
-const search = () => {
+// Api call
+import { fetchApi, baseUrl } from "../utils/fetchApi";
+
+const search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
     
@@ -29,8 +36,41 @@ const search = () => {
                 <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
             {searchFilters && <SearchFilters/>}
+            <Text fontSize='2xl' p='4' fontWeight='bold'>
+                Properties {router.query.purpose}
+            </Text>
+            <Flex flexWrap='wrap'>
+                {properties.map((property) => <Property property={property} key={property.id} />)}
+            </Flex>
+            {properties.length === 0 && (
+                <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5' >
+                    <Image width='280px' height='280px' src={noresult} alt='No Result :(' />
+                    <Text fontSize='xl' marginTop='3'>No Results Found.</Text>
+                </Flex>
+            )}
         </Box>
     )
 }
 
 export default search;
+
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+    return {
+        props: {
+          properties: data?.hits,
+        }
+    }
+}
